b7a36a37acb98bcb2cfda961d89ef4421e2472d4
jinsareader <96166724+jinsareader@users.noreply.github.com>
2024-09-12 12:32:38 +0900

    Add files via upload

---
+++ source/Venv_activator.py
@@ -0,0 +1,19 @@
+import subprocess;
+import sys;
+import os;
+
+
+
+def run_in_venv(script_path):
+    venv_path = os.path.dirname(os.path.abspath(__file__)) + "\\.recycler"
+    # Define the activation command based on the OS
+    activate_cmd = f'{venv_path}/bin/activate' if sys.platform != 'win32' else f'{venv_path}/Scripts/activate'
+
+    # Construct the command to run the script
+    command = f'source {activate_cmd} && python {script_path}' if sys.platform != 'win32' else f'{venv_path}/Scripts/activate && python {script_path}'
+    
+    # Execute the command
+    subprocess.run(command, shell=True, check=True)
+
+
+
