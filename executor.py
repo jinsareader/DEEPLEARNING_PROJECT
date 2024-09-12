fd23c13e4756ed29328918d3df0b3728fdead6f8
jinsareader <96166724+jinsareader@users.noreply.github.com>
2024-09-12 13:48:26 +0900

    Update executor.py

--- executor.py
+++ executor.py
@@ -2,5 +2,9 @@ from source import Venv_activator;
 import os;
 
 
-Venv_activator.run_in_venv(os.path.dirname(os.path.abspath(__file__)) + "\\source\\Mainform.py");
-
+try :
+    Venv_activator.run_in_venv(os.path.dirname(os.path.abspath(__file__)) + "\\source\\Mainform.py");
+except Exception as e :
+    print("\n\n\n--------------------------------오류 발생------------------------------------")
+    print(e);
+    input();
