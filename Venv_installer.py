8e91318054adee1400dbdc666c74bf2b579d192b
jinsareader <96166724+jinsareader@users.noreply.github.com>
2024-09-17 20:05:21 +0900

    Add files via upload

--- Venv_installer.py
+++ Venv_installer.py
@@ -7,6 +7,9 @@ subprocess.run("cd ./source && python -m venv .recycler", shell=True, check=True
 Venv_activator.run_in_venv("-m pip install opencv-python");
 Venv_activator.run_in_venv("-m pip install pillow");
 Venv_activator.run_in_venv("-m pip install tensorflow");
+Venv_activator.run_in_venv("-m pip install torch");
+Venv_activator.run_in_venv("-m pip install torchvision");
+Venv_activator.run_in_venv("-m pip install matplotlib");
 
 
 
--- executor.py
+++ executor.py
@@ -1,10 +1,10 @@
 from source import Venv_activator;
 import os;
 
-
 try :
     Venv_activator.run_in_venv(os.path.dirname(os.path.abspath(__file__)) + "\\source\\Mainform.py");
 except Exception as e :
     print("\n\n\n--------------------------------오류 발생------------------------------------")
     print(e);
     input();
+
